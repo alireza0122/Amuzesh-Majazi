9d7a424caba6f10493ffba1d8e456a7e3cb9e47c
Alireza <your-email@example.com>
2026-08-30 16:36:37 +0330

    08/06 44

---
+++ src/app/view/dashboard/dashboard-banner/dashboard-banner.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-dashboard-banner',
+  styleUrl: './dashboard-banner.css',
+  templateUrl: './dashboard-banner.html',
+})
+export class DashboardBanner {}

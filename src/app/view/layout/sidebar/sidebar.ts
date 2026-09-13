72ffe4ee374567097cc5a98b59058a00135236e6
Alireza <your-email@example.com>
2026-09-13 22:50:06 +0330

    23/06/05

--- src/app/view/layout/sidebar/sidebar.ts
+++ src/app/view/layout/sidebar/sidebar.ts
@@ -15,4 +15,29 @@ export class Sidebar {
   closeMenu() {
     this.closeSidebar.emit();
   }
+
+  isDarkMode = false;
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme === 'dark') {
+      this.isDarkMode = true;
+      document.documentElement.classList.add('dark');
+    }
+  }
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+
+      localStorage.setItem('theme', 'light');
+    }
+  }
 }
